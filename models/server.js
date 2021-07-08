65a921a9680944db03aeb054a0bae75a4c68bd09
Ernesto <ernestopang513@hotmail.com>
2021-07-07 21:10:11 -0500

    conección a la base de datos completada

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 
 
@@ -10,12 +11,20 @@ class Server{
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios'
         
+        //Conectar a base de datos
+        this.conectarDB();
+
         //Middlewares
         this.middlewares();
 
-
+        //Rutas de mi aplicación
         this.routes();
     }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
     middlewares(){
 
         //Lectura y parseo del body
