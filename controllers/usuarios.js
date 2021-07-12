ed981db0a4824d6d10f0ade84b307aea7710b7d1
Ernesto <ernestopang513@hotmail.com>
2021-07-12 01:16:50 -0500

    verificacion de email por express-validator

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,7 @@
 const {response, request} = require('express');
+const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
 const usuariosGet = (req = request, res = response) => {
     
@@ -19,12 +22,33 @@ const usuariosPut = (req, res = response) => {
     });
 }
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async(req, res = response) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json(errors)
+    }
     
-    const body = req.body;
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario({nombre, correo, password, rol});  
+
+    // Verificar que el correo existe
+    const emailExist = await Usuario.findOne({correo});
+    if(emailExist){
+        return res.status(400).json({
+            msg: 'Ese correo ya está registrado'
+        })
+    }
+
+    //Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    //Guardar la base de datos
+    await usuario.save();
     res.status(201).json({
         msg: 'post a mi API - constrolador',
-        body
+        usuario
     });
 }
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,5 +1,6 @@
 
 const {Router} = require('express');
+const { check } = require('express-validator');
 const { usuariosGet, usuariosPut, usuariosDelete, usuariosPost } = require('../controllers/usuarios');
 
 const router = Router();
@@ -9,7 +10,9 @@ router.get('/', usuariosGet );
 //PUT
 router.put('/:id',  usuariosPut);
 //Post
-router.post('/',  usuariosPost);
+router.post('/', [
+    check('correo', 'El correo no es valido').isEmail(),
+], usuariosPost);
 //Delete
 router.delete('/',  usuariosDelete);
 
